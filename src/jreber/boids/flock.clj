62e99c0f1143d8ad2076c812f7704c7938b7bd3f
James Reber <jreber75@gmail.com>
2018-11-04 07:58:28 -0700

    Add Basic Flock Functions

---
+++ src/jreber/boids/flock.clj
@@ -0,0 +1,13 @@
+(ns jreber.boids.flock)
+
+(defprotocol flock
+  "Represents the set of things you cna do with a flock."
+
+  (center-of-mass [this] "Returns the position of the center of mass of the
+  flock.")
+
+  (nearyby-boids [flock radius position] "Returns the set of boids within radius
+  distance of position.")
+
+  (density-gradient [flock position] "Returns the density graident (in some
+  format) of the flock at position."))
