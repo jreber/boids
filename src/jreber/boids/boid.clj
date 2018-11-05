cbc748ebc0027d4ad977bf14cd44d115913d8170
James Reber <jreber75@gmail.com>
2018-11-04 17:35:40 -0700

    Create Boid update Function to Move Boids
    This commit adds the function `update` in the `boid` namespace to
    update a boid in a single time step.
    
    The docstring of `update` explains the purpose of that function. Or
    more interest here is the intent of that function: Having an update
    function that returns an updated boid object, combined with making
    flocks iterable, allows patterns like this to update the flock for a
    single time step:
    
    ```clojure
    (let [boids (boids flock)
          create-boid (...)
          f (fn [boid]
              (update create-boid boid flock))]
      (map f boids)
    ```

--- src/jreber/boids/boid.clj
+++ src/jreber/boids/boid.clj
@@ -1,4 +1,5 @@
-(ns jreber.boids.boid)
+(ns jreber.boids.boid
+  (:refer-clojure :exclude [update]))
 
 (defprotocol boid
   "Basic data algebra for a boid; or, 'the set of all things you can do
@@ -12,3 +13,21 @@
 
   (adjust [boid flock] "Returns an acceleration (in some undetermined format) to
   keep this boid aligned with its flock. A flock is a set/seq of boids."))
+
+(defn update
+  "Returns a new boid object that is is moved one time step according to
+  the values of (position ...), (velocity ...), and (adjust ...).
+
+  The difference between this function and boid#adjust is that adjust
+  returns the acceleration that the boid would do, while this function
+  actually makes that change.
+
+  create-boid must be a function that takes two parameters (position
+  and velocity) and returns a boid."
+  [create-boid boid flock]
+  (let [pos   (position boid)
+        vel   (velocity boid)
+        accel (adjust boid flock)]
+    (create-boid
+     (+ pos vel)
+     (+ vel accel))))
