7c9d66b268de2ba98f04db02979b40ed0da57109
James Reber <jreber75@gmail.com>
2018-11-15 21:08:51 -0700

    Add Concrete Implementation of Vector
    This change adds a concrete implementation of Vector.
    
    More interesting is that this change fully provides spec stuff for
    Vector. The proved very enlightening because it highlighted a weakness
    in the design of Vector: doubles.
    
    1. Doubles have a few weird values: positive infinity, negative
       infinity, and NaN. My `defspec` tests demonstrated that my Vector
       implementation was not correctly handling these values. I ended up
       just excluding those values from the allowed values in Vectors.
    2. My Vector invariants did not correctly handle double rounding
       error. See, e.g., the `magnitude` test.
    
    Also, my `defspec` tests also found that I initially used
    `Math/arctan2` incorrectly, reversing the order of the paramters. Spec
    rocks.

--- src/jreber/boids/math/vector.clj
+++ src/jreber/boids/math/vector.clj
@@ -1,46 +1,45 @@
 (ns jreber.boids.math.vector
   (:refer-clojure :exclude [+ - /])
-  (:require [clojure.algo.generic.arithmetic :as generic :refer [+ - /]]
-            [clojure.spec.alpha :as s]))
+  (:require [clojure.algo.generic
+             [arithmetic :as generic :refer [+ - /]]
+             [math-functions :as m]]
+            [clojure.spec.alpha :as s]
+            [clojure.spec.gen.alpha :as gen]))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; vector definitions
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defprotocol Vector
-  "A representation of a simple vector. Up/down vector? Who cares!"
-  (magnitude [vector] "Returns the magnitude of the vector")
-
-  (angle [vector] "Returns the angle of the vector (in radians? from what
-  zero?).")
-
-  (x [vector] "Returns the x component of the vector, assuming 'x' means
-  something useful.")
-
-  (y [vector] "Returns the y component of the vector, assuming 'y' means
-  something useful.")
-
-  (add [vector other] "Returns a new vector that is the sum of vector and
-  other.")
-
-  (scale [vector scaler] "Returns a new vector that is a scaled version of
-  vector."))
+  "A representation of a simple vector. Double values of
+  infinity (either one) and NaN are unsupported and lead to arbitrary
+  behavior."
+  (magnitude [v] "Returns the magnitude of the vector.")
+  (angle     [v] "Returns the angle of the vector in radians.")
+  (x         [v] "Returns the x component of v, assuming 'x' means something useful.")
+  (y         [v] "Returns the y component of v, assuming 'y' means something useful.")
+  (add       [v other] "Returns a new vector that is the sum of v and other.")
+  (scale     [v scaler] "Returns a new vector that is a scaled version of v."))
 
 (s/def ::vector #(satisfies? Vector %))
+(s/def ::nice-double (s/and float?
+                            #(not (Double/isNaN %))
+                            #(not (Double/isInfinite %))))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; basic vector operations
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(defmethod + [Vector Vector]
+(defmethod + [clojure.lang.PersistentVector clojure.lang.PersistentVector]
   [v1 v2]
   (add v1 v2))
 
-(defmethod - [Vector Vector]
+(defmethod - [clojure.lang.PersistentVector clojure.lang.PersistentVector]
   [v1 v2]
   (let [negate #(scale % -1)]
     (add v1 (negate v2))))
 
-(defmethod / [Vector java.lang.Number]
+(defmethod / [clojure.lang.PersistentVector java.lang.Number]
   [v s]
   (scale v (/ s)))
 
@@ -49,3 +48,34 @@
   [vs]
   (/ (reduce + vs)
      (count vs)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; vector implementations
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(def square #(Math/pow % 2))
+(def sqrt   #(Math/sqrt %))
+(def pi     Math/PI)
+(def pi-half (/ pi 2))
+(def pi-three-halves (* (/ pi 2) 3))
+(def two-pi (* 2 pi))
+(defn arctan [x y]
+  (Math/atan2 y x)) ;; yes, y before x; see javadoc
+
+
+(extend-protocol Vector
+  clojure.lang.PersistentVector
+  (magnitude [[x y]] (sqrt
+                      (+ (square x) (square y))))
+  (angle     [[x y]] (mod
+                      (arctan x y)
+                      two-pi))
+  (x         [[x y]] x)
+  (y         [[x y]] y)
+  (add       [v other] (mapv + v
+                             ((juxt x y) other)))
+  (scale [v scaler] (mapv #(* scaler %) v)))
+
+(s/def ::vector
+  (s/with-gen ::vector
+    #(gen/vector (s/gen ::nice-double)
+                 2 2))) ;; because vectors are now Vectors
---
+++ test/jreber/boids/math/vector_test.clj
@@ -0,0 +1,94 @@
+(ns jreber.boids.math.vector-test
+  (:refer-clojure :exclude [+ - /])
+  (:require [clojure.test :refer :all]
+            [jreber.boids.math.vector :as v]
+            [clojure.algo.generic.arithmetic :refer [+ - /]]
+            [clojure.spec.alpha :as s]
+            [clojure.test.check
+             [generators :as gen]
+             [clojure-test :refer [defspec]]
+             [properties :as prop]]))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; test Vector implementation
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defspec magnitude
+  (prop/for-all [[a b :as v] (s/gen ::v/vector)]
+    (let [c (v/magnitude v)]
+      (and (>= (+ c 1E-100) (max a b)) ;; handle stupid float rounding
+           (not (neg? c))))))
+
+(defspec angle
+  (prop/for-all [[x y :as v] (s/gen ::v/vector)]
+    (let [q (v/angle v)
+          >=zero? #(or (zero? %) (pos? %))
+          <=zero? #(or (zero? %) (neg? %))]
+      (and (<= 0 q v/two-pi)
+           (cond
+             (and (>=zero? x) (>=zero? y)) (<= 0 q v/pi-half) ;; top right quadrant
+             (and (<=zero? x) (>=zero? y)) (<= v/pi-half q v/pi) ;; top left
+             (and (<=zero? x) (<=zero? y)) (<= v/pi q v/pi-three-halves) ;; bottom left
+             (and (>=zero? x) (<=zero? y)) (or (<= v/pi-three-halves q v/two-pi)
+                                               (zero? q)))))))
+
+(defspec x
+  (prop/for-all [[x y :as v] (s/gen ::v/vector)]
+    (= x (v/x v))))
+
+(defspec y
+  (prop/for-all [[x y :as v] (s/gen ::v/vector)]
+    (= y (v/y v))))
+
+(defspec add
+  (prop/for-all [[x1 y1 :as v1] (s/gen ::v/vector)
+                 [x2 y2 :as v2] (s/gen ::v/vector)]
+    (= (v/add v1 v2)
+       [(+ x1 x2)
+        (+ y1 y2)])))
+
+(defspec scale
+  (prop/for-all [[x y :as v] (s/gen ::v/vector)
+                 s (s/gen ::v/nice-double)]
+    (= (v/scale v s)
+       [(* x s)
+        (* y s)])))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; test basic vector operations with [+ - /] and avg
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defspec addition
+  (prop/for-all [[x1 y1 :as v1] (s/gen ::v/vector)
+                 [x2 y2 :as v2] (s/gen ::v/vector)]
+    (= (+ v1 v2)
+       [(+ x1 x2)
+        (+ y1 y2)])))
+
+(defspec subtraction
+  (prop/for-all [[x1 y1 :as v1] (s/gen ::v/vector)
+                 [x2 y2 :as v2] (s/gen ::v/vector)]
+    (= (- v1 v2)
+       [(- x1 x2)
+        (- y1 y2)])))
+
+(defspec scaler-division
+  (prop/for-all [[x y :as v] (s/gen ::v/vector)
+                 s (gen/such-that
+                    (complement zero?)
+                    (s/gen ::v/nice-double))]
+    (= (/ v s)
+       [(/ x s)
+        (/ y s)])))
+
+(defspec avg
+  (prop/for-all [vs (gen/not-empty
+                     (gen/vector
+                      (s/gen ::v/vector)))]
+    (let [xs (map first vs)
+          ys (map second vs)
+          sum (partial reduce +)]
+      (= (v/avg vs)
+         [(/ (sum xs) (count xs))
+          (/ (sum ys) (count ys))]))))
+
